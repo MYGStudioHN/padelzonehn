a8122eaa13b769e8974f55af4a319ec90e183bdb
MYGStudioHN <myg.agenciacreativa@gmail.com>
2026-09-24 19:59:02 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Configuración del lanzador de Padel Zone.
+ *
+ * Este es el ÚNICO archivo que se edita al instalar.
+ * Cuando actualices index.html, este queda intacto.
+ */
+window.PZ_CONFIG = {
+  // La URL de la aplicación web, la que termina en /exec
+  app: "PEGA_AQUI_TU_URL"
+};
